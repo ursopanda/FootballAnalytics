6d5291e769e502559e788b3c6130fa6856918fe9
Emil <e.syundyukov@gmail.com>
2016-01-17 19:43:09 +0200

    Moved JSONParser to void type method

--- app/src/main/java/app/football/lu/footballanalitics/MainActivity.java
+++ app/src/main/java/app/football/lu/footballanalitics/MainActivity.java
@@ -14,6 +14,13 @@ import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import androidsqlite.DatabaseHandler;
+import androidsqlite.Referee;
+import androidsqlite.Team;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initializing method to parse JSON file
+        readJSON("futbols0.json");
 
         // Turnira tabula
         final Button tabulaPoga = (Button) findViewById(R.id.tabulaPoga);
@@ -83,50 +93,94 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Testing JSON Parsing
-        final Button jsonParser = (Button) findViewById(R.id.jsonPoga);
-        jsonParser.setOnClickListener(new View.OnClickListener() {
+        final Button jsonParsing = (Button) findViewById(R.id.jsonPoga);
+        jsonParsing.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
                 startActivity(intent);
             }
         });
+    }
 
-//        TextView output = (TextView) findViewById(R.id.textView1);
+    public void readJSON(String jsonFileName) {
+
+        JSONObject spele = null;
+
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        // Cleaning from old entries
+        db.deleteFootballStats();
+
+//        contactList = new ArrayList<HashMap<String, String>>();
+//        Map teamMap = new HashMap<>();
+        Map<String, Integer> teamMap = new HashMap<>();
+        boolean isExtraPeriod = false;
+
+//        ListView lv = getListView();
+
+        String jsonString;
+        JSONObject jsonObject = null;
+
+//        try {
+//            InputStream inputStream = getAssets().open("elements.json");
+//            int sizeOfJSONFile = inputStream.available();
 //
-//            JSONObject jsonObject;
-//            jsonObject = parseJSONData();
-//            JSONArray jsonArray = null;
+//            //array that will store all the data
+//            byte[] bytes = new byte[sizeOfJSONFile];
+//
+//            //reading data into the array from the file
+//            inputStream.read(bytes);
+//
+//            //close the input stream
+//            inputStream.close();
+//
+//            jsonString = new String(bytes, "UTF-8");
+//            jsonObject = new JSONObject(jsonString);
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        } catch (JSONException x) {
+//            x.printStackTrace();
+//        }
 //
 //        try {
-//            jsonArray = jsonObject.getJSONArray("Komanda");
+//            contacts = jsonObject.getJSONArray("contacts");
 //        } catch (JSONException e) {
 //            e.printStackTrace();
 //        }
-//        String data = "";
-//            for (int i = 0; i < jsonArray.length(); i++) {
 //
-//                JSONObject jsonObject1 = null;
+//        if (contacts != null) {
+//            for (int i=0; i < contacts.length(); i++) {
 //                try {
-//                    jsonObject1 = jsonArray.getJSONObject(i);
+//                    JSONObject c = contacts.getJSONObject(i);
+//
+//                    String id = c.getString("id");
+//                    String name = c.getString("name");
+//                    String email = c.getString("email");
+//                    String address = c.getString("address");
+//                    String gender = c.getString("gender");
+//
+//                    JSONObject phone = c.getJSONObject("phone");
+//                    String mobile = phone.getString("mobile");
+//                    String home = phone.getString("home");
+//                    String office = phone.getString("office");
+//
+//                    HashMap<String,String> contact = new HashMap<String, String>();
+//
+//                    contact.put("id", id);
+//                    contact.put("name", name);
+//                    contact.put("email", email);
+//                    contact.put("mobile", mobile);
+//
+//                    contactList.add(contact);
+//
 //                } catch (JSONException e) {
 //                    e.printStackTrace();
 //                }
-//                String uzvards = jsonObject1.optString("Uzvards").toString();
-//                String vards = jsonObject1.optString("Vards").toString();
-//
-//                data += "Node " + i + " \n Uzvards= " + uzvards + " \n Vards " + vards + "\n";
 //            }
-//            output.setText(data);
+//        }
 
-
-    }
-
-    // Method for reading JSON file from Assets folder
-    public JSONObject parseJSONData() {
-        String jsonString = null;
-        JSONObject jsonObject = null;
         try {
-            InputStream inputStream = getAssets().open("elements.json");
+            InputStream inputStream = getAssets().open(jsonFileName);
             int sizeOfJSONFile = inputStream.available();
 
             //array that will store all the data
@@ -140,16 +194,96 @@ public class MainActivity extends AppCompatActivity {
 
             jsonString = new String(bytes, "UTF-8");
             jsonObject = new JSONObject(jsonString);
-
         } catch (IOException ex) {
             ex.printStackTrace();
-            return null;
-        }
-        catch (JSONException x) {
+        } catch (JSONException x) {
             x.printStackTrace();
-            return null;
-
         }
-        return jsonObject;
+
+        try {
+            spele = jsonObject.getJSONObject("Spele");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if (spele != null) {
+            try {
+                int cardNumber = 0;
+                JSONArray teams = spele.getJSONArray("Komanda");
+
+                for (int i = 0; i < teams.length(); i++) {
+                    // Getting information on teams
+                    JSONObject team = teams.getJSONObject(i);
+                    JSONObject goals = team.getJSONObject("Varti");
+                    JSONArray goal = goals.getJSONArray("VG");
+                    for (int g = 0; g < goal.length(); g++) {
+                        JSONObject currentGoal = goal.getJSONObject(g);
+                        String goalTime = currentGoal.get("Laiks").toString().replaceAll(":", "");
+                        if (Integer.parseInt(goalTime) > 6000) {
+                            isExtraPeriod = true;
+                            break;
+                        }
+                    }
+                    // Getting HashMap: TeamName -> Goals
+                    teamMap.put(team.getString("Nosaukums"), goal.length());
+
+                    // Getting information on cards
+                    JSONObject cardObject = team.getJSONObject("Sodi");
+                    try {
+                        JSONArray cards = cardObject.getJSONArray("Sods");
+                        cardNumber += cards.length();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        cardNumber++;
+                    }
+                }
+
+                // Game analytics - getting info on winner/loser
+                Map.Entry<String,Integer> entry=teamMap.entrySet().iterator().next();
+                String key1 = entry.getKey();
+                String value1 = entry.getValue().toString();
+                String key2 = teamMap.entrySet().iterator().next().getKey();
+                String value2 = teamMap.entrySet().iterator().next().getValue().toString();
+
+                if (Integer.parseInt(value1) > Integer.parseInt(value2)) {
+                    if (!isExtraPeriod) {
+                        db.addTeam(new Team(key1, 1, 0, Integer.parseInt(value1), 5));
+                        db.addTeam(new Team(key2, 0, 1, Integer.parseInt(value2), 1));
+                    }
+                    else {
+                        db.addTeam(new Team(key1, 1, 0, Integer.parseInt(value1), 3));
+                        db.addTeam(new Team(key1, 0, 1, Integer.parseInt(value2), 2));
+                    }
+                }
+                else {
+                    if (!isExtraPeriod) {
+                        db.addTeam(new Team(key2, 1, 0, Integer.parseInt(value2), 5));
+                        db.addTeam(new Team(key1, 0, 1, Integer.parseInt(value1), 1));
+                    }
+                    else {
+                        db.addTeam(new Team(key2, 1, 0, Integer.parseInt(value2), 3));
+                        db.addTeam(new Team(key1, 0, 1, Integer.parseInt(value1), 2));
+                    }
+                }
+
+                // Gathering info about referee
+                JSONObject referee = spele.getJSONObject("VT");
+                // Putting Referee into the DB
+//                    if (!db.checkIsDataAlreadyInDBorNot("referees","name",referee.getString("Vards"))
+//                            && (!db.checkIsDataAlreadyInDBorNot("referees","surname",referee.getString("Surname"))))
+                db.addReferee(new Referee(referee.getString("Vards"),referee.getString("Uzvards"), cardNumber, 1));
+                db.getAllReferees();
+                // If the following referee exists in DB
+//                    else
+//                        db.updateReferee()
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        List allReferees = db.getAllReferees();
+        List allTeams = db.getAllTeams();
+
+        db.close();
     }
 }
